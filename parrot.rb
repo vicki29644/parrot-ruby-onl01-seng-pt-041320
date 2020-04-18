ea6b1c9f7b300559469ef63ac13b4179c24fee64
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-18 02:04:50 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,29 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot (phrase ="Squawk!")
+  puts phrase
+  phrase
+end  
+  
+def double(number)
+  number * 2 
+end
+
+doubled = double(4)
+
+  puts double(doubled)
+
+def speak(phrase)
+
+  puts phrase
+
+end
+
+def shout (sentence)
+  
+  "#{sentence.upcase}!!!!!!"
+end
+
+
+
+
+  
+  
--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -1,4 +1,3 @@
-require_relative './spec_helper'
 require_relative '../parrot.rb' # Code your solution in this file
 
 describe '#parrot' do
